2508f080264bdc8828d550fed12cdfe3e04e1af6
minkhantkyaw <mkhantk102@gmail.com>
2025-01-02 11:53:34 +0630

    First Commit

---
+++ assignment1/four/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "#1d265d",
+        lightAqua: "#6cd4d3",
+        navyDark: "#0f172a",
+        deepNavy: "#030b30",
+        indigoDark: "#1e275e",
+      },
+    },
+  },
+  plugins: [],
+};
