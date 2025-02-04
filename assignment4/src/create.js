d71854c8d5b8bfc65d92826f467f1932a522a9f9
minkhantkyaw <mkhantk102@gmail.com>
2025-02-04 14:27:26 +0630

    Add Create page and function

---
+++ assignment4/src/create.js
@@ -0,0 +1,47 @@
+//variables
+const createForm = document.querySelector("#createForm");
+const afterCreateImg = document.querySelector("#afterCreateImg");
+const url = "http://localhost:3000/posts";
+let createData = {};
+
+window.onload = async () => {
+  const data = await fetch(url);
+  const result = await data.json();
+
+  // console.log(result);
+  let count = result.length;
+
+  createData.id = (count + 1).toString();
+};
+
+createForm.createImg.addEventListener("input", (e) => {
+  afterCreateImg.src = e.target.files[0].name;
+});
+
+createForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  createData.title = createForm.createTitle.value;
+  createData.image_url = createForm.createImg.files[0].name;
+  createData.content = createForm.createContent.value;
+  createData.created_at = new Date().toISOString();
+  createData.created_by = JSON.parse(
+    localStorage.getItem("bloggify_js_app")
+  ).name;
+  // console.log(createData.created_by);
+
+  const resonse = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(createData),
+  })
+    .then((response) => response.json())
+    .then((response) => console.log(resonse))
+    .catch((error) => console.error(error));
+
+  window.location.href = "homepage.html";
+});
+
+// console.log(new Date().toISOString());
